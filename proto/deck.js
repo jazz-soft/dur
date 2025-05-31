0e7040df7a0dd752965352e8d1904bead7572994
Sema <jazz-dev@jazz-soft.net>
2025-05-30 20:51:23 -0400

    Deck.shuffle()

--- proto/deck.js
+++ proto/deck.js
@@ -1,12 +1,37 @@
 const WIDTH = 71;
 const HEIGHT = 96;
 
-function card() {
+function card(n) {
+    var x = 12;
+    var y = 4;
+    if (n >= 0 && n < 36) {
+        x = [5, 6, 7, 8, 9, 10, 11, 12, 0][n % 9];
+        y = [0, 1, 3, 2][Math.floor(n / 9)];
+    }
+    if (n == 36) x = 10;
+    else if (n == 37) x = 11;
     var span = document.createElement('span');
     span.style.display = 'inline-block';
     span.style.backgroundColor = 'red';
     span.style.width = WIDTH + 'px';
     span.style.height = HEIGHT + 'px';
     span.style.backgroundImage = 'url("cards.png")';
+    span.style.backgroundPositionX = (-x * WIDTH) + 'px';
+    span.style.backgroundPositionY = (-y * HEIGHT) + 'px';
     return span;
 }
+
+function Deck() { this.A = [...Array(36).keys()]; }
+Deck.prototype.shuffle = function() {
+    var n = this.A.length;
+    var k, x;
+    while (n) {
+        k = Math.floor(Math.random() * n);
+        n--;
+        x = this.A[k]; 
+        this.A[k] = this.A[n];
+        this.A[n] = x;
+    }
+
+}
+Deck.prototype.card = function(n) { return this.A[n]; }
