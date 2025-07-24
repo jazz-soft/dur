2d7d328532a9e790fe9fc5b4150474519ea03220
Sema <jazz-dev@jazz-soft.net>
2025-07-23 23:01:56 -0400

    Added members

--- tools/state.h
+++ tools/state.h
@@ -6,12 +6,13 @@
 
 class State {
 public:
-    State() : Tr(0) {}
+    State() : Tr(0), St(0) {}
+    State(const State& X) : A(X.A), B(X.B), T(X.T), M(X.M), Tr(X.Tr), St(X.St) {}
     template<typename T1, typename T2, typename T3> State(T1 a, T2 b, T3 t) : A(a), B(b), Tr(t) {}
     friend std::ostream& operator<<(std::ostream& os, const State& X);
 private:
-    Hand A, B, T;
-    unsigned char Tr;
+    Hand A, B, T, M;
+    unsigned char Tr, St;
 };
 
 #endif // _STATE_H_
